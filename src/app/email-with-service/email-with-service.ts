fa7fd4d29eddfb8b7d1c0decccee67c8064cb9d2
Emanuele B. <27818313+Emahhh@users.noreply.github.com>
2025-09-25 12:31:27 +0200

    scaffolding for component

---
+++ src/app/email-with-service/email-with-service.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-with-service',
+  imports: [],
+  templateUrl: './email-with-service.html',
+  styleUrl: './email-with-service.css'
+})
+export class EmailWithService {
+
+}
